7baf6b4f2ed9c2775c2ae3663c52ff480b4491ce
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-22 18:03:02 -0400

    test

---
+++ CliffordAnderson_inf651_midterm_test/api/categories/read_single.php
@@ -0,0 +1,32 @@
+<?php
+  //Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Get ID
+  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+  // Get post
+
+  $post->read_single();
+
+  // Create array
+  $post_arr = array(
+      'id' => $post->id,
+      'quote' => $post->title,
+      'author' => $post->author,
+      'category' => $post->category_id
+  );
+
+  //Make JSON
+  print_r(json_encode($post_arr));
---
+++ CliffordAnderson_inf651_midterm_test/api/quotes/quotes.php
@@ -0,0 +1,58 @@
+<?php
+
+  //Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  
+  include_once '../../config/Database.php';
+  include_once '../../models/Quote.php';
+  include_once '../../models/Category.php';
+  include_once '../../models/Author.php';
+
+  // Instantiate DB & conne/ct
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $quoteObject = new Quote($db);
+
+  // Blog post query
+  $quotation = $quoteObject->read();
+  
+  // Get row count
+  $num = $quotation->rowCount();
+
+  //Check if any posts
+  if($num > 0){
+      // Post array
+      $posts_arr = array();
+      $posts_arr['data'] = array();
+
+      while ($row = $quotation->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $post_item = array(
+           'id' => $id,
+           'quote' => $quote,
+           'authorId' => $authorId,
+           'categoryId' => $categoryId
+           //'body' => html_entity_decode($body),
+           //'author' => $author,
+           //'category' => $category
+        );
+
+        // Push to "data"
+
+        array_push($posts_arr['data'], $post_item);
+    }
+    
+    echo json_encode($posts_arr);
+
+}else{
+
+    // No Posts
+
+    echo json_encode(
+        array('message' => 'No Posts Found')
+    );
+
+  }
