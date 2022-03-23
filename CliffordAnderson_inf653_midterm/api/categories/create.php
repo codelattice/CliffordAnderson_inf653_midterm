1c1473218ce73e89d8560cb3073bf1a20439fa7f
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 02:36:25 -0400

    access control change

--- CliffordAnderson_inf653_midterm/api/categories/create.php
+++ CliffordAnderson_inf653_midterm/api/categories/create.php
@@ -1,8 +1,8 @@
 <?php
   //Headers
-  /*header('Access-Control-Allow-Origin: *'); REMOVING FOR TEST PURPOSES
+  header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: POST');
+  /*header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
   Access-Control-Allow-Methods, Authorization, X-Requested-With');*/
   
