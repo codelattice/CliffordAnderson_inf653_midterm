0c1cd9e97a61fdc8cbe481ae306306c26791bb13
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 01:03:42 -0400

    testing author creation functionality

--- CliffordAnderson_inf653_midterm/api/authors/create.php
+++ CliffordAnderson_inf653_midterm/api/authors/create.php
@@ -7,22 +7,19 @@
   Access-Control-Allow-Methods, Authorization, X-Requested-With');
   
   include_once '../../config/Database.php';
-  include_once '../../models/Post.php';
+  include_once '../../models/Author.php';
 
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
 
   // Instantiate blog post object
-  $post = new Post($db);
+  $post = new Author($db);
 
   // Get raw posted data
 
   $data = json_decode(file_get_contents("php://input"));
 
-  $post->title = $data->title;
-  $post->body = $data->body;
-  $post->author = $data->author;
   $post->category_id = $data->category_id;
 
   //Create post
