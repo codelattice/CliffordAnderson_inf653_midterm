73802e538b4ea16a63c61c975efad91c10372fcd
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 00:55:23 -0400

    testing echo json_encode array

--- CliffordAnderson_inf653_midterm/api/categories/create.php
+++ CliffordAnderson_inf653_midterm/api/categories/create.php
@@ -30,7 +30,8 @@
   //Create post
   if($post->create()){
     echo json_encode(
-      array('message' => 'Post Created')
+      //array('message' => 'Post Created')
+      array('id' => $db->lastInsertId(), 'category' => $post->category
     );
 } else {
       echo json_encode(
