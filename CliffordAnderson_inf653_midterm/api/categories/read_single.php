97fc7bc5e88e244d0b09c0658d155a00d6c6c224
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 13:48:26 -0400

    fine-tuning

--- CliffordAnderson_inf653_midterm/api/categories/read_single.php
+++ CliffordAnderson_inf653_midterm/api/categories/read_single.php
@@ -4,29 +4,37 @@
   header('Content-Type: application/json');
   
   include_once '../../config/Database.php';
-  include_once '../../models/Post.php';
+  include_once '../../models/Category.php';
 
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
 
   // Instantiate blog post object
-  $post = new Post($db);
+  $categorical = new Author($db);
 
   // Get ID
-  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+  $categorical->id = isset($_GET['id']) ? $_GET['id'] : die();
 
   // Get post
 
-  $post->read_single();
+  $categorical->read_single();
 
   // Create array
   $post_arr = array(
-      'id' => $post->id,
-      'quote' => $post->title,
-      'author' => $post->author,
-      'category' => $post->category_id
+      'id' => $categorical->id,
+      'category' => $categorical->category
   );
 
   //Make JSON
-  print_r(json_encode($post_arr));
+  //print_r(json_encode($post_arr));
+
+if($categorical->read_single()){
+    echo json_encode(
+      ($post_array)
+    );
+} else {
+      echo json_encode(
+          array('message' => 'categoryId Not Found')
+      );
+  }
