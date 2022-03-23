0f46331b530c2f47bf63bb21bc54cf069c07e9de
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-22 21:01:46 -0400

    Update index.php

--- CliffordAnderson_inf653_midterm/api/categories/index.php
+++ CliffordAnderson_inf653_midterm/api/categories/index.php
@@ -12,5 +12,5 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 }
 else if ($method === 'GET'){
-    require('read.php);
+    require('read.php');
 }
