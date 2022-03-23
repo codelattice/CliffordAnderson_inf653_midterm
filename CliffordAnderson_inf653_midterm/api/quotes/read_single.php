49795e659dc004ee0d18339e18d4c2084adb878a
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-22 22:53:10 -0400

    mmhmm

--- CliffordAnderson_inf653_midterm/api/quotes/read_single.php
+++ CliffordAnderson_inf653_midterm/api/quotes/read_single.php
@@ -4,14 +4,14 @@
   header('Content-Type: application/json');
   
   include_once '../../config/Database.php';
-  include_once '../../models/Post.php';
+  include_once '../../models/Quote.php';
 
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
 
   // Instantiate blog post object
-  $post = new Post($db);
+  $post = new Quote($db);
 
   // Get ID
   $post->id = isset($_GET['id']) ? $_GET['id'] : die();
