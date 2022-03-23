8e2809bee6803cf49fdeda84f6f4c6acbdd4f5d9
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 00:49:03 -0400

    Update create.php

--- CliffordAnderson_inf653_midterm/api/quotes/create.php
+++ CliffordAnderson_inf653_midterm/api/quotes/create.php
@@ -1,10 +1,10 @@
 <?php
   //Headers
-  header('Access-Control-Allow-Origin: *');
+  /*header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
-  Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  Access-Control-Allow-Methods, Authorization, X-Requested-With');*/
   
   include_once '../../config/Database.php';
   include_once '../../models/Quote.php';
@@ -20,7 +20,7 @@
 
   $data = json_decode(file_get_contents("php://input"));
 
-  $post->id = $data->id;
+  //$post->id = $data->id; commenting out for testing purposes
   $post->quote = $data->quote;
   $post->authorId = $data->authorId;
   $post->categoryId = $data->categoryId;
