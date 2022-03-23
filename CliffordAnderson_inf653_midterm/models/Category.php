41b9002d21121ddd321e0b2586fed97e5046b4e4
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 00:10:26 -0400

    Update Category.php

--- CliffordAnderson_inf653_midterm/models/Category.php
+++ CliffordAnderson_inf653_midterm/models/Category.php
@@ -62,17 +62,11 @@
       public function create(){
         //Create query
         $query = 'INSERT INTO '.$this->table.'
-          SET
+          VALUES
              category = :category';
           
           //Prepare statement
         $stmt = $this->conn->prepare($query);
-
-        //Clean data
-        /*$this->title = htmlspecialchars(strip_tags($this->title));
-        $this->body = htmlspecialchars(strip_tags($this->body));
-        $this->author = htmlspecialchars(strip_tags($this->author));
-        $this->category_id = htmlspecialchars(strip_tags($this->category_id));*/
 
         //Bind data
         $stmt->bindParam(':category', $this->category);
