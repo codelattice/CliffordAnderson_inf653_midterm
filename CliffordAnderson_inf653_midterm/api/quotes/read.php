de28fe1c5877bff4ffd7e56949cd8bb4c7aee4b1
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 02:06:15 -0400

    isset

--- CliffordAnderson_inf653_midterm/api/quotes/read.php
+++ CliffordAnderson_inf653_midterm/api/quotes/read.php
@@ -6,6 +6,10 @@
   include_once '../../config/Database.php';
   include_once '../../models/Quote.php';
 
+  if (isset(id)){
+    require 'read_single.php';
+  }
+
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
