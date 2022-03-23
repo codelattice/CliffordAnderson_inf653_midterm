2632324905d01e5b4385ee54972fcf976d2e4200
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-23 02:37:35 -0400

    access control uncommented in full

--- CliffordAnderson_inf653_midterm/api/categories/create.php
+++ CliffordAnderson_inf653_midterm/api/categories/create.php
@@ -2,9 +2,9 @@
   //Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  /*header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
-  Access-Control-Allow-Methods, Authorization, X-Requested-With');*/
+  Access-Control-Allow-Methods, Authorization, X-Requested-With');
   
   include_once '../../config/Database.php';
   include_once '../../models/Category.php';
