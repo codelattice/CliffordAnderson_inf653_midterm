7f313c8aa4708f5dbfb68e42b3791d78b9a958f0
codelattice <89589147+codelattice@users.noreply.github.com>
2022-03-24 00:07:04 -0400

    Update index.php

--- CliffordAnderson_inf653_midterm/api/categories/index.php
+++ CliffordAnderson_inf653_midterm/api/categories/index.php
@@ -30,3 +30,36 @@ if ($method === 'POST'){
 if ($method === 'PUT'){
     require('update.php');
 }
+
+<?php
+
+include('../../config/Database.php');
+
+$database = new Database();
+$db = $database->connect();
+require('../../models/Category.php');
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}
+
+if ($method === 'GET'){
+    if (isset($_GET['id']) ){
+        require('read_single.php');
+    }
+    else{
+        require('read.php');
+    }
+}
+
+if ($method === 'POST'){
+    require('create.php');
+}
+
+if ($method === 'PUT'){
+    require('update.php');
+}
